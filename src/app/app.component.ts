e0455017b5b13d6d05be008d69661338198dc022
Parker <parkerjlandon@gmail.com>
2020-07-13 01:55:09 -0700

    lesson 7 code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,20 +4,15 @@ import { Component, Inject } from '@angular/core';
   selector: 'app-root',
   template: 
   `<div>
-    <app-simple-form></app-simple-form>
-    {{mail.message}}
-    <hr>
-    {{api}}
+    <ul>
+      <li *ngFor="let message of mail.messages">
+        {{message}}
+      </li>
+    </ul>
   </div>`,
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = `Let's get started`;
-
   constructor(
     @Inject('mail') public mail,
-    @Inject('api') public api,
-  ){
-
-  }
+  ){}
 }
--- src/app/mail.service.ts
+++ src/app/mail.service.ts
@@ -4,6 +4,10 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MailService {
-  message = "You've got mail!"
+  messages = [
+    `You're now friends with John`,
+    'John liked your tweet',
+    `You'll never believe what John said...`
+  ]
   constructor() { }
 }
