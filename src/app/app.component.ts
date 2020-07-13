3761b1be41db5f9d0e9595337f362010a39ef946
Parker <parkerjlandon@gmail.com>
2020-07-13 04:26:20 -0700

    lesson 10 code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,23 @@ import { Component, Inject } from '@angular/core';
   template: 
   `<div>
     <ul>
+      <li *ngFor="let message of mail.messages">{{ message.text }}</li>
+    </ul>
       <app-simple-form 
       *ngFor="let message of mail.messages"
-      [message]="message"
+      [message]="message.text"
+      (update)="onUpdate(message.id, $event.text)"
       >
 
       </app-simple-form>
-    </ul>
   </div>`,
 })
 export class AppComponent {
+
+  onUpdate(id, text){
+    this.mail.update(id, text)
+  }
+
   constructor(
     @Inject('mail') public mail,
   ){}
