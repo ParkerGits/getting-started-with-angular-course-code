ff585412ccd2da670cba472fda8a41e14f2265d6
Parker <parkerjlandon@gmail.com>
2020-07-13 13:57:24 -0700

    lesson 12 code

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -3,26 +3,37 @@ import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 @Component({
   selector: 'app-simple-form',
   template: `
-  <input #myInput type="text" [(ngModel)]="message"/>
+  <input 
+    #myInput 
+    type="text" 
+    [(ngModel)]="message"
+    [ngClass]="{mousedown:isMousedown}"
+    (mousedown)="isMousedown = true"
+    (mouseup)="isMousedown = false"
+    (mouseleave)="isMousedown  = false"
+  >
   <button (click)="update.emit({text:message})">Click me!</button>
   `,
   styles: [`
-  :host{
-    display: flex;
-    flex-direction: column;
-  }
-  
-  input:focus{
-    font-weight: bold;
-    outline: none;
-  }
-  
-  button{
-    border:none;
-  }
-  `]
+    :host{
+      display: flex;
+      flex-direction: column;
+    }
+    .mousedown{
+      border: 2px solid green;
+    }
+    input:focus{
+      font-weight: bold;
+      outline: none;
+    }
+    button{
+      border: none;
+    }
+    `]
 })
 export class SimpleFormComponent implements OnInit {
+  
+  isMousedown;
 
   @Input() message
 
