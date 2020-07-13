c2fa139d8edb48bc99cfaf43e59de012a80c27eb
Parker <parkerjlandon@gmail.com>
2020-07-13 01:29:05 -0700

    lesson 6 code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
-import { MailService } from './mail.service';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div><app-simple-form></app-simple-form>{{mail.message}}</div>`,
+  template: 
+  `<div>
+    <app-simple-form></app-simple-form>
+    {{mail.message}}
+    <hr>
+    {{api}}
+  </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = `Let's get started`;
 
-  constructor(private mail:MailService){
+  constructor(
+    @Inject('mail') public mail,
+    @Inject('api') public api,
+  ){
 
   }
 }
